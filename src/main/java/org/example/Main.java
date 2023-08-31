5ba38c33e440bdeb7f01493004d6ae16bc81d881
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-08-31 20:35:45 +0200

    Using the ArrayList

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,30 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        String bird = "tweety";
+        String dog = "wuffi";
+        String cat = "mutzi";
+        String ant = "helpi";
+
+        List<String> animals = new ArrayList<>();
+        animals.add(bird);
+        animals.add(dog);
+        animals.add(cat);
+        animals.add(ant);
+
+        String favoriteAnimal = animals.get(0);
+        System.out.println(favoriteAnimal);
+
+        int numberOfAnimals = animals.size();
+        System.out.println("I have " + numberOfAnimals + " animals");
+
+        for (String anAnimal : animals) {
+            System.out.println("I feed " + anAnimal);
+        }
     }
 }
