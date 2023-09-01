e4615606ae218c086491872b5aa4f959153aae21
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-09-01 10:13:30 +0200

    Live coding ffm-java-23-1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,31 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        String atHome = "Miele";
+        String spontaneous = "Xiaomi";
+        String inGarden = "Bosch";
+        String inCar = "Aldi";
+
+        List<String> vacuums = new ArrayList<>();
+        vacuums.add(atHome);
+        vacuums.add(spontaneous);
+        vacuums.add(inGarden);
+        vacuums.add(inCar);
+
+        System.out.println(vacuums);
+
+        String favoriteVacuum = vacuums.get(0);
+        System.out.println(favoriteVacuum);
+
+        for (String vacuum : vacuums) {
+            System.out.println("Ich staubsauge mit " + vacuum);
+        }
+
+
     }
 }
